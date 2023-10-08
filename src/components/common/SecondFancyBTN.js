7bdf2f47460ea78923e3f4c2505db1e0404db917
Sadak Vali <54495800+SadakVali@users.noreply.github.com>
2023-10-08 06:13:24 +0530

    Started Manager Route Yesterday

---
+++ src/components/common/SecondFancyBTN.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { ReactComponent as RightArrow } from "../../assets/Icons/RightArrow.svg";
+
+const FirstFancyBTN = ({ text, onClick }) => {
+  return (
+    <button
+      className="bg-[#4960F9] text-white rounded-3xl h-[5.25rem] text-[1.25rem]
+      flex items-center justify-center relative gap-4 hover:cursor-pointer min-w-[24rem]
+      overflow-hidden px-8 max-w-fit text-2xl"
+      style={{ boxShadow: "8px 8px 8px 0px rgba(27, 57, 255, 0.20)" }}
+      onClick={onClick}
+      type="submit"
+    >
+      <p className="font-montserrat">{text}</p>
+      <RightArrow />
+      <div
+        className="w-[9rem] aspect-square absolute rounded-full 
+        right-0 bottom-0 translate-x-[30%] translate-y-[65%] bg-gradient-to-t 
+        from-[#C72FF800] to-[#C72FF8]"
+      ></div>
+      <div
+        className="w-[9rem] aspect-square absolute rounded-full 
+        left-0 top-0 -translate-x-[30%] -translate-y-[65%] bg-[#3AF9EF]"
+      ></div>
+    </button>
+  );
+};
+
+export default FirstFancyBTN;
--- src/pages/ManagerHome.js
+++ src/pages/ManagerHome.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+import SecondFancyBTN from "../components/common/SecondFancyBTN";
+
 const ManagerHome = () => {
-  return <div>ManagerHome</div>;
+  return (
+    <div
+      className="my-16 w-11/12 max-w-maxContentTab mx-auto flex flex-col 
+      justify-center items-center gap-y-16"
+    >
+      <div
+        className="text-gradient w-full text-[2.5rem] flex flex-col items-center 
+        justify-center"
+      >
+        <p className="font-extrabold font-accent">
+          Simplify Your Booking Experience &
+        </p>
+        <p className="font-extrabold font-accent">
+          Stress-Free Reservations for Managers
+        </p>
+      </div>
+      <SecondFancyBTN text="Fetch Bookings" />
+    </div>
+  );
 };
 
 export default ManagerHome;
