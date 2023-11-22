d834b41ca5d43e99c70da8dfd46a663436b573fe
Sadak Vali <54495800+SadakVali@users.noreply.github.com>
2023-11-22 09:56:27 +0530

    one more attempt to solve the scrolling issue in the mobile phones

--- src/components/core/Home/ManagerHome/ManagerHome.js
+++ src/components/core/Home/ManagerHome/ManagerHome.js
@@ -56,36 +56,43 @@ const ManagerHome = () => {
 
   return (
     <div
-      className="min-h-[75vh] w-11/12 max-w-maxContentTab mx-auto flex flex-col
-      justify-center items-center gap-y-16"
+      className="min-h-[70vh] h-full w-11/12 max-w-maxContentTab mx-auto flex flex-col
+      justify-center items-center"
     >
-      <BookingCalendar setDateRange={setDateRange} dateRange={dateRange} />
+      {dateRange && dateRange.length === 0 && (
+        <BookingCalendar setDateRange={setDateRange} dateRange={dateRange} />
+      )}
       {/* Check in & check time time pickers */}
       {dateRange && dateRange.length > 0 && (
-        <div className="flex gap-16 flex-col sm:flex-row">
-          <div className="flex flex-col justify-center items-center gap-8">
-            <p className="text-[1.25rem] text-[#4135F3] font-montserrat">
-              Check In Time
-            </p>
-            <TimePicker setState={setCheckInTime} />
+        <div
+          className="absolute left-0 right-0 flex flex-col 
+          justify-center items-center gap-y-16"
+        >
+          <div className="flex gap-16 flex-col sm:flex-row">
+            <div className="flex flex-col justify-center items-center gap-8">
+              <p className="text-[1.25rem] text-[#4135F3] font-montserrat">
+                Check In Time
+              </p>
+              <TimePicker setState={setCheckInTime} />
+            </div>
+            <div className="flex flex-col justify-center items-center gap-8">
+              <p className="text-[1.25rem] text-[#4135F3] font-montserrat">
+                Check Out Time
+              </p>
+              <TimePicker setState={setCheckOutTime} />
+            </div>
           </div>
-          <div className="flex flex-col justify-center items-center gap-8">
-            <p className="text-[1.25rem] text-[#4135F3] font-montserrat">
-              Check Out Time
-            </p>
-            <TimePicker setState={setCheckOutTime} />
+          <div
+            className={`${
+              checkInTime !== null && checkOutTime !== null
+                ? ""
+                : "pointer-events-none opacity-50"
+            } `}
+          >
+            <SecondFancyBTN text="Next" onClick={onClickHandler} />
           </div>
         </div>
       )}
-      <div
-        className={`${
-          checkInTime !== null && checkOutTime !== null
-            ? ""
-            : "pointer-events-none opacity-50"
-        } `}
-      >
-        <SecondFancyBTN text="Next" onClick={onClickHandler} />
-      </div>
     </div>
   );
 };
